45fe4c9edcc8a00dfa2f5331ddd88da428758eac
beniusis <rokasbeniu@gmail.com>
2024-05-07 19:16:05 +0300

    feat: created a header that is shared between all pages

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,34 @@
 <script setup>
-import { RouterView } from 'vue-router';
+import { RouterLink, RouterView } from 'vue-router';
 </script>
 
 <template>
+  <header>
+    <h1><RouterLink class="link" to="/">gamefindr</RouterLink></h1>
+  </header>
   <RouterView />
 </template>
+
+<style scoped>
+header {
+  width: 100%;
+  height: 10vh;
+}
+
+h1 {
+  font-size: 3rem;
+  font-weight: 900;
+  line-height: 1;
+  text-align: center;
+  transition: font-size 300ms ease-in-out;
+
+  @media (max-width: 576px) {
+    font-size: 2rem;
+  }
+}
+
+.link {
+  color: var(--clr-brandy-600);
+  text-decoration: none;
+}
+</style>
