8e7d55aa9763c18838fea1559e877b7d9dd4b29b
beniusis <rokasbeniu@gmail.com>
2024-05-08 18:42:03 +0300

    impr: updated the home view page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,28 @@
+<script setup>
+import { useGames } from '@/composables/games';
+import LoadingState from '@/components/LoadingState.vue';
+import ErrorMessage from '@/components/ErrorMessage.vue';
+import GameBlock from '@/components/GameBlock.vue';
+
+const { loading, error, games } = useGames();
+</script>
+
 <template>
-  <div></div>
+  <main>
+    <LoadingState v-if="loading" />
+    <ErrorMessage v-else-if="!loading && error" />
+    <div v-else class="container">
+      <GameBlock v-for="game in games" :game="game" />
+    </div>
+  </main>
 </template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+}
+</style>
