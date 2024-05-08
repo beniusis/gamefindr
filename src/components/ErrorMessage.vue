c696457950abccca94999b3243a2e88cf7802611
beniusis <rokasbeniu@gmail.com>
2024-05-08 18:41:21 +0300

    feat: created an ErrorMessage component

---
+++ src/components/ErrorMessage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="error">Sorry, something went wrong! Try refreshing the page.</div>
+</template>
+
+<style scoped>
+.error {
+  font-size: 2rem;
+  font-weight: 800;
+  color: var(--clr-brandy-900);
+  text-align: center;
+  user-select: none;
+  transition: font-size 300ms ease-in-out;
+
+  @media (max-width: 576px) {
+    font-size: 1.25rem;
+  }
+}
+</style>
