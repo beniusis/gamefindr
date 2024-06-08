a432412f3c506107e176f619f4ebd1631d00c5bd
beniusis <rokasbeniu@gmail.com>
2024-06-08 22:04:51 +0300

    refactor: move header to its own component

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,9 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router';
+import Header from './components/Header.vue';
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
-  <header>
-    <h1><RouterLink class="link" to="/">gamefindr</RouterLink></h1>
-  </header>
+  <Header />
   <RouterView />
 </template>
-
-<style scoped>
-header {
-  width: 100%;
-  height: 10vh;
-}
-
-h1 {
-  font-size: 3rem;
-  font-weight: 900;
-  line-height: 1;
-  text-align: center;
-  transition: font-size 300ms ease-in-out;
-
-  @media (max-width: 576px) {
-    font-size: 2rem;
-  }
-}
-
-.link {
-  color: var(--clr-brandy-900);
-  text-decoration: none;
-  transition: all 300ms ease-out;
-
-  &:hover {
-    color: var(--clr-brandy-400);
-  }
-}
-</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <header>
+    <h1><a class="link" href="./">gamefindr</a></h1>
+  </header>
+</template>
+
+<style scoped>
+header {
+  width: 100%;
+  height: 10vh;
+}
+
+h1 {
+  font-size: 3rem;
+  font-weight: 900;
+  line-height: 1;
+  text-align: center;
+  transition: font-size 300ms ease-in-out;
+
+  @media (max-width: 576px) {
+    font-size: 2rem;
+  }
+}
+
+.link {
+  color: var(--clr-brandy-900);
+  text-decoration: none;
+  transition: all 300ms ease-out;
+
+  &:hover {
+    color: var(--clr-brandy-400);
+  }
+}
+</style>
